6fdf4f05eb255e27a4fc0483de0f84ce32f60c09
GROOTScorer <gaeldlk@gmail.com>
2023-09-25 13:58:26 -0300

    fix linter warnings

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <div className='container'>
       <header className="Blog-header">
         <h3>AssemBlog</h3>
-        <a href="https://github.com/GROOTScorer/blog-tp"><img className='github' width="25px" height="25px" title="Repositorio" src="https://cdn-icons-png.flaticon.com/256/25/25231.png"></img></a>
+        <a href="https://github.com/GROOTScorer/blog-tp"><img alt="" className='github' width="25px" height="25px" title="Repositorio" src="https://cdn-icons-png.flaticon.com/256/25/25231.png"></img></a>
       </header>
 
       <Prevista />
--- src/Prevista.js
+++ src/Prevista.js
@@ -3,12 +3,12 @@ import './Prevista.css';
 function Prevista(){
     return(
         <div className='prevista'>
-            <img className='imgprev' src='https://la35.net/assets/img/binary.png'></img>
+            <img alt="" className='imgprev' src='https://la35.net/assets/img/binary.png'></img>
             <h3 className='tituloprev'>El Titulo</h3>
             <p className='sintesisprev'>Esta sintesis habla un poco sobre lo que se va a ver en el artículo.</p>
             <p className='autorprev'>Por Groot</p>
-            <img height="25px" width="25px" className='like' src='https://cdn-icons-png.flaticon.com/256/833/833472.png'></img>
-            <img height="25px" width="25px" className='copylink' src='https://cdn.iconscout.com/icon/free/png-256/free-chain-link-connection-attach-37919.png'></img>
+            <img alt="" height="25px" width="25px" className='like' src='https://cdn-icons-png.flaticon.com/256/833/833472.png'></img>
+            <img alt="" height="25px" width="25px" className='copylink' src='https://cdn.iconscout.com/icon/free/png-256/free-chain-link-connection-attach-37919.png'></img>
 
         </div>
     )
